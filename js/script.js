dbe53175080458c0be701b3d831fa60d8b608f53
Monsur1993 <morshed.bubt@gmail.com>
2022-10-31 23:32:14 +0600

    Add job project 1 st commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+    $(".owl-carousel").owlCarousel({
+        autoplay : true ,
+        autoplayHoverPause : false,
+        autoplayTimeout: 2000,
+        loop:true , 
+        dots : false ,
+        items : 1,
+        touchDrag:false,
+        dotsContainer:false
+
+    });
+
+    
